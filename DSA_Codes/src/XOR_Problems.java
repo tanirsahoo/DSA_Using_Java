e64104b5fd4b3709260289a9449e189b1542d02f
tanirsahoo <tanir.sahoo.2021@gmail.com>
2023-11-18 23:12:59 +0530

    updates

--- DSA_Codes/src/XOR_Problems.java
+++ DSA_Codes/src/XOR_Problems.java
@@ -20,7 +20,30 @@ class operations{
     }
 
     void two_non_repeating(int[] ar){
-        
+        int v = 0 ;
+        for(int i = 0 ; i < ar.length ; i ++)
+            v = v ^ ar[i] ;
+        if(find(0) == 1){
+            int k1 = 0 , k2 = 0 ;
+            for(int i = 0 ; i < ar.length ; i ++)
+            {
+                if(find(i) == 0)
+                    k1 = k1 ^ ar[i] ;
+                if(find(i) == 1)
+                    k2 = k2 ^ ar[i] ;
+            }
+            System.out.println("The two numbers are: " + k1 + " and " + k2);
+        }
+        else{
+
+        }
+    }
+    int find(int n){ // Find the n th bit in the given number a
+        int k = 1 << n ; // We are shifting the value by n bits and creating a mask and then performing and operation to check whether the bit present at i'th position in the original number is 1 or 0.
+        if((a & k) > 0)
+            return 1 ;
+        else
+            return 0 ;
     }
 
 }
