5b1176842434eafef696fed1c9a5d7534462e0f7
tanirsahoo <tanir.sahoo.2021@gmail.com>
2023-12-31 19:22:43 +0530

    Updates

--- DSA_Codes/src/Buy_and_Sell_Stock.java
+++ DSA_Codes/src/Buy_and_Sell_Stock.java
@@ -1,5 +1,6 @@
 /*
 Problem statement number 1: In this problem we need to buy a stock and sell it so that we make the maximum profit out of the transaction.
+Problem statement number 2: In this problem we can buy and sell as many stocks I want and I need to figure out the maximum profit by performing this activity.
 */
 package DSA_Codes.src;
 
@@ -18,6 +19,16 @@ class buy_sell_stock_1{
     }
 }
 
+class buy_sell_stock_2{
+    public int max_profit(int ar[]){
+        int profit = 0 ;
+        for(int i = 1 ; i < ar.length ; i ++){
+            if(ar[i] > ar[i-1])
+                profit = profit + (ar[i-1] - ar[i]) ;
+        }
+        return profit ;
+    }
+}
 public class Buy_and_Sell_Stock {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in) ;
@@ -28,7 +39,9 @@ public class Buy_and_Sell_Stock {
             System.out.println("Enter the number at index: " + i);
             ar[i] = sc.nextInt() ;
         }
-        buy_sell_stock_1 ob = new buy_sell_stock_1() ;
-        System.out.println("The Maximum profit gained is: " + ob.max_profit(ar)) ;
+//        buy_sell_stock_1 ob = new buy_sell_stock_1() ;
+//        System.out.println("The Maximum profit gained is: " + ob.max_profit(ar)) ;
+        buy_sell_stock_2 ob2 = new buy_sell_stock_2() ;
+        System.out.println("The Maximum profit gained is: " + ob2.max_profit(ar));
     }
 }
