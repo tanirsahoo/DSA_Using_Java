fb5bdaee4e5fd48a6723db4592e260fb311d98b5
tanirsahoo <tanir.sahoo.2021@gmail.com>
2023-12-31 10:20:21 +0530

    Updates

---
+++ DSA_Codes/src/N_Queen_and_Sudoku_Solver.java
@@ -0,0 +1,136 @@
+/*
+1. N-Queen Problem: N-Queen problem states that we need to place N-Queens in an N X N matrix such that no queen interrupts the movement of other queen.
+2. Sudoku Solver:
+*/
+package DSA_Codes.src;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+class N_Queen_Problem{
+    public boolean isSafe(int arr[][] , int row , int col , int N){
+        for(int i = 0 ; i <= row ; i ++){
+            if(arr[i][col] == 1)
+                return false ;
+        }
+        int i = row;
+        int j = col;
+        while(i >= 0 && j >= 0){
+            if(arr[i][j] == 1){
+                return false ;
+            }
+            i -- ;
+            j -- ;
+        }
+        i = row ;
+        j = col ;
+        while(i >= 0 && j < N){
+            if(arr[i][j] == 1){
+                return false ;
+            }
+            i -- ;
+            j ++ ;
+        }
+        return true ;
+    }
+    public boolean nQueen(int arr[][] , int row , int N){
+        if(row >= N)
+            return true ;
+        for(int i = 0 ; i < N ; i ++){
+            if(isSafe(arr , row , i , N)) {
+                arr[row][i] = 1;
+                if (nQueen(arr, row + 1, N))
+                    return true;
+                arr[row][i] = 0;
+            }
+//            display(arr , N) ;
+        }
+        return false ;
+    }
+    public void display(int arr[][] , int n){
+//        int j = 0 ;
+        System.out.println("------------------------------------------");
+        for(int i = 0 ; i < n ; i ++){
+            for(int j = 0 ; j < n ; j ++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("------------------------------------------");
+    }
+}
+
+class Sudoku_Solver{
+    public boolean isSafe(int arr[][] , int row , int col , int N , int num){
+        for(int i = 0 ; i < N ; i ++){
+            if(arr[i][col] == num)
+                return false ;
+        }
+        for(int j = 0 ; j < N ; j ++){
+            if(arr[row][j] == num)
+                return false ;
+        }
+        int array_row = row - (row % 3) ;
+        int array_col = col - (col % 3) ;
+        for(int i = 0 ; i < 3 ; i ++)
+            for(int j = 0 ; j < 3 ; j ++){
+                if(arr[i + array_row][j + array_col] == num)
+                    return false ;
+            }
+        return true ;
+    }
+    public boolean sudoku(int arr[][] , int row , int col , int N){
+        if(row == N-1 && col == N)
+            return true ;
+        if(col == N){
+            row ++ ;
+            col = 0 ;
+        }
+        if(arr[row][col] != 0)
+            return sudoku(arr , row , col + 1 , N) ;
+        for(int num = 1 ; num < 10 ; num ++){
+            if(isSafe(arr , row , col , N , num)) {
+                arr[row][col] = num;
+                if(sudoku(arr , row , col + 1 , N))
+                    return true ;
+            }
+            arr[row][col] = 0 ;
+        }
+        return false ;
+    }
+    void display_matrix(int arr[][] , int N){
+        for(int i = 0 ; i < N ; i ++) {
+            for (int j = 0; j < N; j++)
+                System.out.print(arr[i][j] + " ");
+            System.out.println();
+        }
+    }
+}
+public class N_Queen_and_Sudoku_Solver {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in) ;
+//        System.out.println("Enter the value of n");
+//        int n = sc.nextInt() ;
+//        int[][] arr = new int[n][n] ;
+//        for(int i = 0 ; i < n ; i ++){
+//            for(int j = 0 ; j < n ; j ++){
+//                arr[i][j] = 0 ;
+//            }
+//        }
+//        N_Queen_Problem ob = new N_Queen_Problem() ;
+//        if(ob.nQueen(arr , 0 , n))
+//            ob.display(arr , n) ;
+        int[][] arr = { {3, 0, 6, 5, 0, 8, 4, 0, 0},
+                        {5, 2, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 8, 7, 0, 0, 0, 0, 3, 1},
+                        {0, 0, 3, 0, 1, 0, 0, 8, 0},
+                        {9, 0, 0, 8, 6, 3, 0, 0, 5},
+                        {0, 5, 0, 0, 9, 0, 6, 0, 0},
+                        {1, 3, 0, 0, 0, 0, 2, 5, 0},
+                        {0, 0, 0, 0, 0, 0, 0, 7, 4},
+                        {0, 0, 5, 2, 0, 6, 3, 0, 0} } ;
+        Sudoku_Solver ob1 = new Sudoku_Solver() ;
+        if(ob1.sudoku(arr , 0 , 0 , 9))
+            ob1.display_matrix(arr , 9);
+    }
+}
--- DSA_Codes/src/Permutation_string.java
+++ DSA_Codes/src/Permutation_string.java
@@ -8,7 +8,7 @@
            ab   a  b   ""     -->> Adding b or "" to every element
           /\   /\  /\   /\
       abc  ab ac a bc b c ""  -->> Adding c or "" to every element
-3. Print all permutations of a given string: 
+3. Print all permutations of a given string: Find all the combinations that could be framed from a given string.
 */
 package DSA_Codes.src;
 
@@ -32,19 +32,42 @@ class string_work{
         power_set_string(src , index + 1 , cr + src.charAt(index));
         power_set_string(src , index + 1 , cr);
     }
+    public String swap(String a , int l , int f){
+        String new_string = "" ;
+        for(int i = 0 ; i < a.length() ; i ++){
+            if(i == l)
+                new_string = new_string + a.charAt(f) ;
+            else if(i == f)
+                new_string = new_string + a.charAt(l) ;
+            else
+                new_string = new_string + a.charAt(i) ;
+        }
+        return new_string ;
+    }
+    public void permute(String a , int f , int l){
+        if(l == f){
+            System.out.println(a);
+        }
+        for(int i = f ; i <= l ; i ++){
+            a = swap(a , (f - 1) , (i - 1)) ;
+            permute(a , f+1 , l);
+            a = swap(a , (f - 1) , (i - 1)) ;
+        }
+    }
 }
 public class Permutation_string {
     public static void main(String[] args) {
         string_work ob = new string_work() ;
         Scanner sc = new Scanner(System.in) ;
-        System.out.println("Enter the String.");
-        String a = sc.nextLine() ;
+//        System.out.println("Enter the String.");
+//        String a = sc.nextLine() ;
 //        int len = a.length() ;
 //        boolean result = ob.palindrome_string(a , len - 1 , 0) ;
 //        if(result == true)
 //            System.out.println("Palindrome String");
 //        else
 //            System.out.println("Not a palindrome String");
-        ob.power_set_string(a , 0 , "");
+//        ob.power_set_string(a , 0 , "");
+        ob.permute("abc" , 1 , 3); // f = first position (Always 1) and l = Last Position(Length of the String)
     }
 }
