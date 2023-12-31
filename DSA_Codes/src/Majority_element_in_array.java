daa0cf097962aeab3d95f87cda1a401500d9633c
tanirsahoo <tanir.sahoo.2021@gmail.com>
2023-12-31 11:25:28 +0530

    Updates

--- DSA_Codes/src/Majority_element_in_array.java
+++ DSA_Codes/src/Majority_element_in_array.java
@@ -9,6 +9,17 @@ import java.util.Scanner;
 public class Majority_element_in_array {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in) ;
-        
+        System.out.println("Enter the number of elements you want to have in the array.");
+        int n = sc.nextInt() ;
+        int[] ar = new int[n] ;
+        for(int i = 0 ; i < n ; i ++){
+            System.out.println("Enter the element at index: " + i);
+            ar[i] = sc.nextInt() ;
+        }
+        int start_index = 0 ;
+        int cnt = 1 ;
+        for(int i = 0 ; i < n ; i ++){
+            
+        }
     }
 }
