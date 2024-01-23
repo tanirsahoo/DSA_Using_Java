0663cda4955308066df7ebec15d1558ead33b411
tanirsahoo <tanir.sahoo.2021@gmail.com>
2024-01-23 13:48:49 +0530

    Updates

--- DSA_Codes/src/Binary_search.java
+++ DSA_Codes/src/Binary_search.java
@@ -13,6 +13,19 @@ class search_algo{
         }
         return -1 ;
     }
+    public int find_element_index_recursion(int ar[] , int n , int lb , int ub){
+        if(lb > ub)
+            return -1 ;
+        else {
+                int mb = (lb + ub) / 2;
+                if (ar[mb] == n)
+                    return mb;
+                else if (ar[mb] > n)
+                    return find_element_index_recursion(ar, n, lb, mb - 1);
+                else
+                    return find_element_index_recursion(ar, n, mb + 1, ub);
+        }
+    }
 }
 public class Binary_search {
     public static void main(String[] args) {
@@ -21,7 +34,8 @@ public class Binary_search {
         System.out.println("Enter the element that you want to search.");
         int n = sc.nextInt() ;
         search_algo ob = new search_algo() ;
-        int index = ob.find_element_index(ar , n) ;
+//        int index = ob.find_element_index(ar , n) ;
+        int index = ob.find_element_index_recursion(ar , n , 0 , ar.length) ;
         if(index == -1)
             System.out.println("No such element exist in this array.");
         else
