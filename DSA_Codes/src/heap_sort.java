7f07af930bdf06b4e147468a306da5b1d750ffa5
tanirsahoo <tanir.sahoo.2021@gmail.com>
2024-04-11 14:50:25 +0530

    Updates

---
+++ DSA_Codes/src/heap_sort.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class heap_sort_algorithm{
+    public int[] heapify(int[] ar, int index , int size){
+        int left_child = 2 * index ;
+        int right_child = 2 * index + 1 ;
+        int larger_index = ar[left_child] > ar[right_child] ? (2 * index) : (2 * index + 1) ;
+        while((left_child >= size || right_child >= size) && (ar[larger_index] > ar[index])){
+            int swap_var = ar[larger_index] ;
+            ar[larger_index] = ar[index] ;
+            ar[index] = swap_var ;
+            index = larger_index ;
+        }
+        return ar ;
+    }
+}
+
+public class heap_sort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in) ;
+        System.out.println("Enter the number of elements you want to have inside the array.");
+        int n = sc.nextInt() ;
+        int[] ar = new int[n] ;
+        for(int i = 0 ; i < n ; i ++){
+            System.out.println("Enter the element at index: " + (i + 1));
+            ar[i] = sc.nextInt() ;
+        }
+        heap_sort_algorithm ob = new heap_sort_algorithm() ;
+        for(int i = (int)(Math.floor(n / 2)) ; i >= 1 ; i --){
+            ar = ob.heapify(ar , i , n) ;
+        }
+    }
+}
