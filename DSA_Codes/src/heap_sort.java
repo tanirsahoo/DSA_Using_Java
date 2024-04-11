a34cb7b53ea6a9fb62dd5bfa60b66b671ac7aa76
tanirsahoo <tanir.sahoo.2021@gmail.com>
2024-04-11 16:20:00 +0530

    Updates

--- DSA_Codes/src/heap_sort.java
+++ DSA_Codes/src/heap_sort.java
@@ -1,3 +1,5 @@
+//Applying heap sort algorithm to sort an array in descending order
+
 import java.util.Scanner;
 
 class heap_sort_algorithm{
@@ -6,16 +8,26 @@ class heap_sort_algorithm{
         int right_child = 2 * index + 1 ;
         if(left_child <= size && right_child <= size){
             int larger_index = ar[left_child] > ar[right_child] ? (2 * index) : (2 * index + 1) ;
-            int swap_var = ar[larger_index] ;
-            ar[larger_index] = ar[index] ;
-            ar[index] = swap_var ;
-            return heapify(ar , larger_index , size) ;
+            if(ar[index] < ar[larger_index]) {
+                int swap_var = ar[larger_index];
+                ar[larger_index] = ar[index];
+                ar[index] = swap_var;
+                return heapify(ar, larger_index, size);
+            }
+            else{
+                return ar ;
+            }
         }
         else if(left_child <= size){
-            int swap_var = ar[left_child] ;
-            ar[left_child] = ar[index] ;
-            ar[index] = swap_var ;
-            return heapify(ar , left_child , size) ;
+            if(ar[index] < ar[left_child]) {
+                int swap_var = ar[left_child];
+                ar[left_child] = ar[index];
+                ar[index] = swap_var;
+                return heapify(ar, left_child, size);
+            }
+            else{
+                return ar ;
+            }
         }
         else{
             return ar ;
@@ -35,8 +47,8 @@ class heap_sort_algorithm{
         return ar ;
     }
     public void display(int[] ar , int n){
-        for(int i = 1 ; i <= n ; i ++){
-            System.out.print(ar[i]);
+        for(int i = n ; i > 0 ; i --){
+            System.out.print(ar[i] + " ");
         }
         System.out.println();
     }
@@ -45,18 +57,18 @@ class heap_sort_algorithm{
 public class heap_sort {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in) ;
-//        System.out.println("Enter the number of elements you want to have inside the array.");
-//        int n = sc.nextInt() ;
-//        int[] ar = new int[n + 1] ;
-//        ar[0] = -1 ;
-//        for(int i = 1 ; i <= n ; i ++){
-//            System.out.println("Enter the element at index: " + i);
-//            ar[i] = sc.nextInt() ;
-//        }
-        int n = 9 ;
-        int ar[] = {-1 , 2 , 5 , 9 , 6 , 3 , 10 , 4 , 19 , 8} ;
+        System.out.println("Enter the number of elements you want to have inside the array.");
+        int n = sc.nextInt() ;
+        int[] ar = new int[n + 1] ;
+        ar[0] = -1 ;
+        for(int i = 1 ; i <= n ; i ++){
+            System.out.println("Enter the element at index: " + i);
+            ar[i] = sc.nextInt() ;
+        }
+//        int ar[] = {-1 , 6 , 8 , 2 , 4 , 16 , 19 , 13 , 11 , 20} ;
+//        int n = ar.length - 1;
         heap_sort_algorithm ob = new heap_sort_algorithm() ;
-        ar = ob.heapsort(ar , n + 1) ;
-        ob.display(ar , n + 1);
+        ar = ob.heapsort(ar , n) ;
+        ob.display(ar , n);
     }
 }
