c216ac999b2794449278f49def190bb073df5af0
tanirsahoo <tanir.sahoo.2021@gmail.com>
2024-04-11 15:21:05 +0530

    Updates

--- DSA_Codes/src/heap_sort.java
+++ DSA_Codes/src/heap_sort.java
@@ -5,14 +5,20 @@ class heap_sort_algorithm{
         int left_child = 2 * index ;
         int right_child = 2 * index + 1 ;
         int larger_index = ar[left_child] > ar[right_child] ? (2 * index) : (2 * index + 1) ;
-        while((left_child >= size || right_child >= size) && (ar[larger_index] > ar[index])){
+        while((left_child <= size || right_child <= size) && (ar[larger_index] > ar[index])){
             int swap_var = ar[larger_index] ;
             ar[larger_index] = ar[index] ;
             ar[index] = swap_var ;
             index = larger_index ;
+            left_child = 2 * index ;
+            right_child = 2 * index + 1 ;
+            larger_index = ar[left_child] > ar[right_child] ? (2 * index) : (2 * index + 1) ;
         }
         return ar ;
     }
+//    public int heapsort(int[] ar){
+//
+//    }
 }
 
 public class heap_sort {
@@ -20,14 +26,19 @@ public class heap_sort {
         Scanner sc = new Scanner(System.in) ;
         System.out.println("Enter the number of elements you want to have inside the array.");
         int n = sc.nextInt() ;
-        int[] ar = new int[n] ;
-        for(int i = 0 ; i < n ; i ++){
-            System.out.println("Enter the element at index: " + (i + 1));
+        int[] ar = new int[n + 1] ;
+        ar[0] = -1 ;
+        for(int i = 1 ; i <= n ; i ++){
+            System.out.println("Enter the element at index: " + i);
             ar[i] = sc.nextInt() ;
         }
         heap_sort_algorithm ob = new heap_sort_algorithm() ;
         for(int i = (int)(Math.floor(n / 2)) ; i >= 1 ; i --){
             ar = ob.heapify(ar , i , n) ;
         }
+        for(int i = 1 ; i <= n ; i ++){
+            System.out.print(ar[i] + " ");
+        }
+        System.out.println();
     }
 }
