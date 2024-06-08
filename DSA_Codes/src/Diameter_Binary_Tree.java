5c599438ab02feae9f0348187e5b220bb0a9e349
tanirsahoo <tanir.sahoo.2021@gmail.com>
2024-06-08 16:11:21 +0530

    Updates

---
+++ DSA_Codes/src/Diameter_Binary_Tree.java
@@ -0,0 +1,36 @@
+public class Diameter_Binary_Tree {
+    public static void main(String[] args) {
+        node_tree<Integer> head = new node_tree<>(15) ;
+        node_tree<Integer> nd1 = new node_tree<>(20) ;
+        node_tree<Integer> nd2 = new node_tree<>(19) ;
+        node_tree<Integer> nd3 = new node_tree<>(42) ;
+        node_tree<Integer> nd4 = new node_tree<>(26) ;
+        node_tree<Integer> nd5 = new node_tree<>(35) ;
+        node_tree<Integer> nd6 = new node_tree<>(38) ;
+        node_tree<Integer> nd7 = new node_tree<>(29) ;
+
+        head.left_child = nd1 ;
+        head.right_child = nd2 ;
+
+        nd1.left_child = nd3 ;
+        nd1.right_child = nd4 ;
+
+        nd2.left_child = nd5 ;
+        nd2.right_child = nd6 ;
+
+        nd3.left_child = nd7 ;
+        nd3.right_child = null ;
+
+        nd4.left_child = null ;
+        nd4.right_child = null ;
+
+        nd5.left_child = null ;
+        nd5.right_child = null ;
+
+        nd6.left_child = null ;
+        nd6.right_child = null ;
+
+        nd7.left_child = null ;
+        nd7.right_child = null ;
+    }
+}
