79e6983be790e194ee88d3882d3397c0d513b8b6
tanirsahoo <tanir.sahoo.2021@gmail.com>
2024-07-11 16:12:10 +0530

    Updates

--- DSA_Codes/src/Java_Recap/Nested_Class.java
+++ DSA_Codes/src/Java_Recap/Nested_Class.java
@@ -6,21 +6,29 @@ public class Nested_Class {
     Nested_Class(){
         System.out.println("This is the demo for Nested Class");
     }
-    class inner_class extends Nested_Class{
+    class inner_class{ // This is a inner class declared inside the class Nested_Class
         inner_class(){
             System.out.println("I am inside the inner_class now");
         }
-        private static int factorial(int n){
+        protected static int factorial(int n){
             if(n == 0 || n == 1)
                 return 1 ;
             else
                 return n * factorial(n - 1) ;
         }
-        int v = 5 ;
+        public static void display_weirdos(){ // This function would be modified to understand the working of an anonymous class.
+            System.out.println("Here I am gonna display this weird String.");
+        }
     }
 
     public static void main(String[] args) {
         Nested_Class obb = new Nested_Class() ;
-//        Nested_Class o = new Nested_Class.inner_class() ;
+        Nested_Class.inner_class o = obb.new inner_class(){ // This is an Anonymous Class
+            public static void display_weirdos(){
+                System.out.println("Here is the modified String that I am going to display inside the anonymous class.");
+            }
+        } ;
+        o.display_weirdos();
+        System.out.println("The factorial of 10 is: " + o.factorial(10)) ;
     }
 }
